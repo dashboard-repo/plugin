c7f2086932559eeadd3122ac25da27e623e745d1
Cody Collicott <cody@dashboard.ca>
2015-10-05 10:08:22 -0400

    v1.3

--- archive-affliate.php
+++ archive-affliate.php
@@ -1,6 +1,33 @@
 <?php
+/**
+ * GitHub Updater
+ *
+ * @package   GitHub_Updater
+ * @author    Cody Collicott
+ * @license   GPL-2.0+
+ * @link      https://github.com/dashboard-repo/plugin
+ */
+
 /*
-Plugin Name: Example Plugin
+Plugin Name:       Example Plugin
+Plugin URI:        https://github.com/dashboard-repo/plugin/releases
+Description:       tester
+Version:           1.0
+Author:            Cody Collicott
+License:           GNU General Public License v2
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+Domain Path:       /languages
+Text Domain:       github-updater
+Network:           true
+GitHub Plugin URI: https://github.com/afragen/github-updater
+GitHub Branch:     develop
+Requires WP:       3.8
+Requires PHP:      5.3
 */
+
+/*
+ * Exit if called directly.
+ * PHP version check and exit.
+ */
 
 echo 'This is an update!';
